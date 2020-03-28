2bd5b822c1bdbddbfe87d8597b836966203d5732
Nesreen <nesreentrgalal@hotmail.com>
2020-03-28 00:08:51 -0400

    P3: changed the code completly since it wasnt work (took like 5 hrs) so i changed it into this , using keyframes to make animations (css)

--- assigments/project3/js/retro.js
+++ assigments/project3/js/retro.js
@@ -1,22 +1,15 @@
 
-/*Downloaded from https://www.codeseek.co/aderaaij/retro-80andaposs-style-grid-and-logo-amqoVJ */
-var grid = document.querySelector('.m-grid');
-var tl = new TimelineMax();
+const grid = document.querySelector('.m-grid');
+const tl = new TimelineMax();
 
 TweenLite.set(grid, {
   transformPerspective: 400,
-  transformOrigin: '50% 50%'
-});
+  transformOrigin: '50% 50%' });
 
-var anim2Props = {
-  rotationX: 75,
-  y: '0%',
-  ease: Power2.easeIn,
-  transformPerspective: 300,
-  onComplete: function onComplete() {
-    return grid.classList.add('is-animating');
-  }
 
-};
-
-tl.to(grid, 1, { scaleY: 1.5, ease: Power3.easeIn }).to(grid, 1, anim2Props, '+=0.3').to('.m-logo__wrap', 1, { scale: 1 });
+tl.
+to(grid, 1, { scaleY: 1, ease: Power3.easeIn }).
+to(grid, 1, { rotationX: 75, y: '0%', ease: Power2.easeIn, transformPerspective: 300, onComplete: () => grid.classList.add('is-animating') }, '+=0.3')
+// .to(grid, 1, {transformPerspective: 300}, '+=1')
+// .to(grid, 1, { ease:Linear.easeNone, onComplete: () => grid.classList.add('is-animating')}, '-=0.3')
+.to('.m-logo__wrap', 1, { scale: 1 });
