b49e298b976c871fc66a0684a8fece724107be49
Nesreen <nesreentrgalal@hotmail.com>
2020-04-14 15:17:15 -0400

    P3: added questions will be adding more ..thinking

--- assigments/project3/js/script.js
+++ assigments/project3/js/script.js
@@ -24,83 +24,10 @@ $(document).ready(function() {
 // This function gets called by getJSON when the data has been loaded.
 // The data itself will be in the 'data' argument as a JavaScript object.
 function gotData(data) {
-  // Now we select random elements from the three arrays inside
-  // our JSON to get a random condiment, cat, and room. Then we add those
-  // words onto our page by setting the text of the appropriate span.
-
-  // First the condiment
-  // Get a random condiment from the condiments array in the JSON
-  let condiment = getRandomElement(data.condiments);
-  // Assume it's singular
-  let verb = 'is';
-  // Check if the last latter of the condiment is an 's'
-  if (condiment.charAt(condiment.length - 1) === 's') {
-    // If so, assume it's plural (this is a flawed assumption)
-    verb = 'are';
-  }
-
-  // Now the cat
-  let cat = getRandomElement(data.cats);
-  let catArticle = "a";
-  //this fixes the indefinite article for the vowels
-  if (
-    cat.charAt(0).toLowerCase() === "a" ||
-    cat.charAt(0).toLowerCase() === "e" ||
-    cat.charAt(0).toLowerCase() === "i" ||
-    cat.charAt(0).toLowerCase() === "o" ||
-    cat.charAt(0).toLowerCase() === "u"
-  ) {
-    catArticle = "an";
-  }
-
-  // Same again for room
-  let room = getRandomElement(data.rooms);
-  let roomArticle = "a";
-  //this fixes the indefinite article for the vowels
-  if (
-    room.charAt(0).toLowerCase() === "a" ||
-    room.charAt(0).toLowerCase() === "e" ||
-    room.charAt(0).toLowerCase() === "i" ||
-    room.charAt(0).toLowerCase() === "o" ||
-    room.charAt(0).toLowerCase() === "u"
-  ) {
-    roomArticle = "an";
-  }
-
-  // New dataset art movements that ends with isms
-  let ism = getRandomElement(data.isms);
-  let ismArticle = "a";
-  //this fixes the indefinite article for the vowels
-  if (
-    ism.charAt(0).toLowerCase() === "a" ||
-    ism.charAt(0).toLowerCase() === "e" ||
-    ism.charAt(0).toLowerCase() === "i" ||
-    ism.charAt(0).toLowerCase() === "o" ||
-    ism.charAt(0).toLowerCase() === "u"
-  ) {
-    ismArticle = "an";
-  }
-
-  // New dataset names that are monsters names
-  let monster = getRandomElement(data.names);
-  let monsterArticle = "a";
-  //this fixes the indefinite article for the vowels
-  if (
-    monster.charAt(0).toLowerCase() === "a" ||
-    monster.charAt(0).toLowerCase() === "e" ||
-    monster.charAt(0).toLowerCase() === "i" ||
-    monster.charAt(0).toLowerCase() === "o" ||
-    monster.charAt(0).toLowerCase() === "u"
-  ) {
-    monsterArticle = "an";
-  }
-  // the description with a template string with the values and variables created
-  let description = `${condiment} and ${ismArticle} ${ism} ${verb} like ${catArticle} ${cat} in ${roomArticle} ${room} because of ${monsterArticle} ${monster}.`;
-  // add it to the page
-  $('body').append(description)
-
-
+  let philosophicalquestions = getRandomElement(data.philosophicalquestions);
+   $('body').append(philosophicalquestions)
 }
+
 
 // dataError()
 //
