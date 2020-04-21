4f9e2ee8de41dc171639846fdb12831ac2352c48
Nesreen <nesreentrgalal@hotmail.com>
2020-04-20 20:38:06 -0400

    P3: final commit and hope for the best :)

--- assigments/project3/js/script.js
+++ assigments/project3/js/script.js
@@ -4,7 +4,7 @@ Project 3: Philopsical Simulation
 
 This Project is inspired by philosophical idealogies like extensionalism according to wiki its a philosophical theory or
 approach which emphasizes the existence of the individual person as a free and responsible agent determining their own development
- through acts of the will, transhumanism which is the possibility of humans subconcious to be in transplanted in the computer
+through acts of the will, transhumanism which is the possibility of humans subconcious to be in transplanted in the computer
 and retro futurism, as well as Walter Benjamin's The Work of Art in the Age of Mechanical Reproduction, the idea of copies, like replicants
 in Blade Runner. Blade Runner's Voight-Kampff Test and Hal from 2001 Space Odyssey were inspirations for this project.
 Retro Futurism/Cyperpunk vibes adds to the retro futurism aesthetics that I was going for.Instead of the human asking
